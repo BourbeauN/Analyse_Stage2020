ca248e1a235bc4f7ff677f004cbe780c507652f6
BourbeauN <naomi.bourbeau@hotmail.com>
2020-07-10 15:19:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import torch 
 import pdb
+import os
+import numpy as np
+import matplotlib.pyplot as plt
 
+
+## New functions
+def load_data():
+    path = 'C:/Users/Naomi/Documents/Université/Stage 2020 - Ahmad Hamdan/Analyse/5kv_500ns-picpic'
+    f = []
+    
+    for filename in os.listdir(path)[:10]:
+        
+        f.append(np.loadtxt(filename,skiprows=12))
+
+    return f
+
+##
+
+########This is a debugger
 def main():
-    pdb.set_trace()
-    print('hello world')
-
+        data = load_data()
+        pdb.set_trace()
+        
 main()
-
-
-
