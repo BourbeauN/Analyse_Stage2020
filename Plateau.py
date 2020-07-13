e996e4fa42f9ca24cb7e212c19fbea4f0f3d463e
Léonard Sauvé <leonardsauve@gmail.com>
2020-07-12 22:13:17 -0400

    debugged !

--- Plateau.py
+++ Plateau.py
@@ -9,11 +9,14 @@ def find_plateau(voltage, time, tresh):
         begin = np.where(voltage == np.ndarray.max(voltage))[0][0] ### to be validated ( tested on 10 )
 
         ## End of plateau phase ##
+        for i in range(begin, len(voltage)):
+            dist = 5
+            if np.abs(voltage[i] - voltage[i-dist]) > tresh:
+                return time[begin], time[i + begin]
         
-        for i, volt in enumerate(voltage[begin:]):
-            t = 100 # play with this val 
-            if np.abs(volt - voltage[i-1]) > t :
-                return time[begin], time[i]
+        #for i, volt in enumerate(voltage[begin:]):
+        #    if np.abs(volt - voltage[begin + i - 1]) > tresh :
+        #        return time[begin], time[i + begin]
 
             
 
@@ -23,37 +26,39 @@ def load_data(tresh):
     #pdb.set_trace()
     List_Plateau = []
     
-    for j in os.listdir(path):
+    for j in sorted(os.listdir(path)):
         time,voltage,current = np.array(np.loadtxt(os.path.join(path, j), dtype = float, delimiter = ',', skiprows = 12, unpack = True))
-        
-        
-        begin, end = find_plateau(voltage, time, tresh)    
+        print(j) 
+        try :  
+            begin, end = find_plateau(voltage, time, tresh)    
+        except TypeError: 
+            print ('unable to find plateau')
+            begin, end  = float('nan'), float('nan')
         plateau = end - begin
         
         List_Plateau.append(plateau)
-        
         
     Final_Plateau = np.asarray(List_Plateau)
     
     Num_Discharges = np.linspace(0,len(Final_Plateau), len(Final_Plateau))    
     return Num_Discharges , Final_Plateau
-
 def plot_data(Num_Discharges, List_Plateau, tresh):
     plt.figure(1)
-    plt.plot(Num_Discharges, List_Plateau,'ko', markersize = 2)
+    plt.plot(Num_Discharges, np.log(List_Plateau),'ko', markersize = 2)
     plt.ylabel('Plateau duration (s)')
     plt.xlabel('Discharge ID')
-    plt.savefig('FIGURES/plateau_size_discharge_id_scatter_plot_t={}.pdf'.format(tresh))
-    plt.savefig('FIGURES/plateau_size_discharge_id_scatter_plot_t={}.png'.format(tresh))
+    plt.savefig('FIGURES/plateau_size_discharge_id_scatter_plot_t={}_log.pdf'.format(tresh))
+    plt.savefig('FIGURES/plateau_size_discharge_id_scatter_plot_t={}_log.png'.format(tresh))
 
 
 
-tresh = 100
-Num_Discharges, List_Plateau = load_data(tresh)
-  
-## PLOTS ##
+tresh = [50,75,100]
 
-plot_data(Num_Discharges, List_Plateau, tresh)
+for t in tresh: 
+    Num_Discharges, List_Plateau = load_data(t)
+    ## PLOTS ##
+
+    plot_data(Num_Discharges, List_Plateau, t)
 
 # b = np.linspace(0, len(time), len(time))
 # plt.plot(b, voltage)
