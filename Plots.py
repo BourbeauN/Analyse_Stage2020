f7d735f7efc735a07849806587e2f526c61413d4
Léonard Sauvé <leonardsauve@gmail.com>
2020-07-16 19:43:27 -0400

    last commit of the day

---
+++ Plots.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import pdb
+
+def main():
+    infile = 'OUT/OUT_PLATEAUS_20kv_100ns_picpic.csv'
+    data = pd.read_csv(infile)
+    pdb.set_trace()
+
+
+main()
