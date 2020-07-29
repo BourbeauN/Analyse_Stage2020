36a1c14f4fb41b90d4652708c75c2b853b67e2f7
BourbeauN <naomi.bourbeau@hotmail.com>
2020-07-29 11:24:32 -0400

    Update Plots.py

--- Plots.py
+++ Plots.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import pdb
+import seaborn as sns
 
 def main():
     infile = 'OUT/OUT_PLATEAUS_20kv_100ns_picpic.csv'
@@ -10,3 +11,11 @@ def main():
 
 
 main()
+
+def ScatterPlot():
+    #nao_path = "/Users/Naomi/Documents/GitHub/Analyse_Stage2020/OUT"
+    #leo_path = "5kv_100nspicpic"
+    path = nao_path
+    
+    for i in file 
+    filename, time_delta, v_delta, Plateau, Plateau_Count = np.load()    
