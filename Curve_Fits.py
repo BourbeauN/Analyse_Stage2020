945c2ef6149526b71accc9c785eaa2193f9acb02
BourbeauN <naomi.bourbeau@hotmail.com>
2020-08-07 15:28:03 -0400

    Update Curve_Fits.py

--- Curve_Fits.py
+++ Curve_Fits.py
@@ -34,7 +34,7 @@ def get_elapsed_time(fnames):
     
     for d in range(len(datetimes)): 
         
-	time_deltas[d] = (datetimes[d] - datetimes[0]).total_seconds()
+        time_deltas[d] = (datetimes[d] - datetimes[0]).total_seconds()
    
    #Tracks seconde part of time stamp
     print("for loop to obtain time stamp complete...")                 
