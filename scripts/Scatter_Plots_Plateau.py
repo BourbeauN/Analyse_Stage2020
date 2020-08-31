e5b6d2609bba83a1053325b6e1bdd0a2f381137e
BourbeauN <naomi.bourbeau@hotmail.com>
2020-08-31 14:13:59 -0400

    Update Scatter_Plots_Plateau.py

--- scripts/Scatter_Plots_Plateau.py
+++ scripts/Scatter_Plots_Plateau.py
@@ -29,17 +29,21 @@ def get_elapsed_time(fnames):
     #Allows to track the first part of the time stamp 
     print("for loop to separate time string complete ...")
     
+    #Converting time difference to seconds
     for d in range(len(datetimes)): 
         
         time_deltas[d] = (datetimes[d] - datetimes[0]).total_seconds()
+        
+        #print to follow evolution of code during execution
         if d%50 == 0 :
             print(time_deltas[d])
    
    #Tracks seconde part of time stamp
     print("for loop to obtain time stamp complete...")                 
 
-    return time_deltas
+    return time_deltas,datetimes
 
+#/Get the parameters of the experience analyzed for plot title
 def get_experiment_name(folder_name):
     
     tension = folder_name.split("_")[3]
@@ -60,12 +64,51 @@ def main():
     
     Results = pd.read_csv(args.INFILE)
     fname =  Results['Filename']
-    ET_file = get_elapsed_time(fname)
+    ET_file,timestamps = get_elapsed_time(fname)
     Plateau = Results['Plateau']
     tension, pulsewidth, configuration, medium = get_experiment_name(args.INFILE)
     
+    #Create list to append filtered data
+    ET,Max_Voltage_Fin = [],[]
+        
+    #List of folders in need of data filtering
+    ## Manually append when there are new folders to filter
+    ## Add corresponding file filter to TimeStamp_Filter list with the same position
+    Data_Filter = ["TAB_PLATEAU_VOLTDIS/VOLT_DIS_20kv_500ns_point-point_water_5000dv_15dk.csv","TAB_PLATEAU_VOLTDIS/VOLT_DIS_5kv_500ns_point-point_water_3000dv_15dk.csv","TAB_PLATEAU_VOLTDIS/VOLT_DIS_20kv_500ns_point-point_heptane_5000dv_15dk.csv"]
+
+    #File from which to start analyzing
+    ##Certain experiments have saved old data in the folder with the new data
+    TimeStamp_Filter = ["20200630101319295","20200821110000743","20200703110232131"]
+
+    filename = str(args.INFILE)
+    
+    ##If the experiment doesnt need to be filtered, this step is to assign a baseline value to the filter
+    file_filter = fname[0]
+    
+    #To filter through the files in need of filtering and changing file_filter with the TimeStamp_Filter value associated with the filtered infolder
+    for i in range(len(Data_Filter)):
+        
+        if filename == Data_Filter[i]:
+            file_filter = TimeStamp_Filter[i]
+    
+    #Transforming the file_filter to be analyzed with datetime
+    timetemp = file_filter.split("_")[-1].split(".csv")[0]
+    
+    #converting str to float with numerical value
+    timethresh = datetime.strptime(timetemp,"%Y%m%d%H%M%S%f")
+
+    #Filtering of files in analyzed folder
+    for i in range(len(timestamps)):
+        if timestamps[i] >= timethresh:
+            ET.append(ET_file[i])
+            Max_Voltage_Fin.append(Plateau[i])
+    
+    #Transforming final lists of data to array
+    ET = np.asarray(ET)
+    Max_Voltage_Fin = np.asarray(Max_Voltage_Fin)
+    
     #Present an explicit error message
-    if len(Plateau) != len (ET_file):
+    if len(Plateau) != len (ET):
         print("array lengths dont match")
     
     ###PLOTS###
