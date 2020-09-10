a5f4bb50b82e2cdb64ad7cf91b081f1aa8ae22ec
BourbeauN <naomi.bourbeau@hotmail.com>
2020-09-10 16:05:47 -0400

    Create Integration.py

---
+++ scripts/Integration.py
@@ -0,0 +1,85 @@
+import pdb
+import numpy as np
+import scipy as sc
+import os
+import argparse
+import pandas as pd
+
+def load_data(filename):
+
+    #loading data    
+    Results = pd.read_csv(filename, skiprows = 10)
+    
+    #creating arrays for time, voltage and current
+    time = Results["TIME"]
+    voltage = Results["CH1"]
+    current = Results["CH2"]
+    
+    return time, voltage, current 
+
+def Trapeze_Integration(ydata,xdata,dx):
+    Integrate_Trap = np.trapz(ydata,xdata,dx)
+    
+    return Integrate_Trap
+
+def Simpson_Integration(ydata,xdata,dx):
+    Integrate_Simp = sc.integrate.simps(ydata,xdata,dx)
+    
+    return Integrate_Simp
+
+def Romberg_Integration(ydata,dx):
+    Integrate_Romb = sc.integrate.romb(ydata,dx)
+    
+    return Integrate_Romb
+
+def Integration(path,dv,dk):
+    
+    # list of discharge files  
+    files = sorted(os.listdir(path))
+    
+    CURR_TO_INT, TIME_TO_INT = [], []
+    
+    TRAP, SIMP, ROMB = [], [], []
+    
+    for i,f in enumerate(files) :
+        
+        time, voltage, current = load_data(os.path.join(path,f))
+        
+        for k in range(dk, len(time)) :
+            if (voltage[k-dk] - voltage[k]) > dv:
+                index = k
+        
+        for j in range(index, len(time)) :
+            TIME_TO_INT.append(time[j])
+            CURR_TO_INT.append(np.abs(current[j]))
+       
+        TRAP.append([f,Trapeze_Integration(CURR_TO_INT,TIME_TO_INT,(1/len(TIME_TO_INT)))])
+        SIMP.append([f,Simpson_Integration(CURR_TO_INT,TIME_TO_INT,(1/len(TIME_TO_INT)))])
+        ROMB.append([f,Romberg_Integration(CURR_TO_INT,(1/len(TIME_TO_INT)))])
+
+    return np.asarray(TRAP),np.asarray(SIMP), np.asarray(ROMB)
+
+def main():
+    
+    ###PARSER###
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', dest = 'INFOLDER', help = 'file folder corresponding to experimental set with discharge infos')
+    parser.add_argument('-dv',type = int,  dest = 'VOLTAGE_THRESHOLD', help = 'pick a value for voltage threshold')
+    parser.add_argument('-dk',type = int,  dest = 'INDEX_THRESHOLD', help = 'pick a value for time threshold')
+    args = parser.parse_args()
+    outfile = args.INFOLDER.split('/')[-1] 
+    
+    dk = args.INDEX_THRESHOLD
+    dv = args.VOLTAGE_THRESHOLD
+    
+    TRAP_TAB, SIMP_TAB, ROMB_TAB = Integration(args.INFOLDER,dv,dk)
+    
+    print("Finished appending, saving tables...")
+    
+    pd.DataFrame(TRAP_TAB, columns = ['Filename', 'Integration']).to_csv(os.path.join('Injected_Charges/Trapeze',"{}.csv".format(outfile)))
+    pd.DataFrame(SIMP_TAB, columns = ['Filename', 'Integration']).to_csv(os.path.join('Injected_Charges/Simpson',"{}.csv".format(outfile)))
+    pd.DataFrame(ROMB_TAB, columns = ['Filename', 'Integration']).to_csv(os.path.join('Injected_Charges/Romberg',"{}.csv".format(outfile)))
+    
+main()
+        
+    
