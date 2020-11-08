4b954b2066e563e3909dcaa781be3ad6af0e563e
nbourbeau <naomi.bourbeau@hotmail.com>
2020-11-08 12:32:03 -0500

    Data_Limits.py still bugged, backing it up before leaving

--- scripts/Data_Limits.py
+++ scripts/Data_Limits.py
@@ -4,28 +4,36 @@ import argparse
 import pandas as pd
 import os
 
-def Limit_Whole(Data,a):
-    index = np.arange(0,(len(Data)-(a-1)),a)
-    Max_array, Min_array = [],[]
+def Limit_Whole(x_data,y_data,a):
+    index = np.arange(0,(len(y_data)-(a-1)),a)
+    Max_yarray, Min_yarray = [],[]
+    Max_xarray, Min_xarray = [],[]
+
     #pdb.set_trace()
     for i in index:
         #pdb.set_trace()
-        temp = Data[i:i+(a-1)]
-        Max_array.append(np.float(np.max(temp)))
-        Min_array.append(np.float(np.min(temp)))
+        temp = y_data[i:i+(a-1)]
+        Max_yarray.append(np.float(np.max(temp)))
+        Max_xarray.append(np.float(x_data[np.where(temp==np.max(temp))]))
+        Min_yarray.append(np.float(np.min(temp)))
+        Min_xarray.append(np.float(x_data[np.where(temp==np.min(temp))]))
     
-    return Max_array,Min_array
+    return Max_yarray,Min_yarray,Max_xarray,Min_xarray
 
-def Limit_Partial(Data,a,Max_array,Min_array):
+def Limit_Partial(x_data,y_data,a,Max_xarray,Min_xarray,Max_yarray,Min_yarray):
    
-    Max_array.append(np.float(np.max(Data)))
-    Min_array.append(np.float(np.min(Data)))
-    
-    return Max_array,Min_array
+    Max_yarray.append(np.float(np.max(Data)))
+    Max_xarray.append(np.float(x_data[np.where(y_data==np.max(y_data))]))
+    Min_yarray.append(np.float(np.min(Data)))
+    Min_xarray.append(np.float(x_data[np.where(y_data==np.min(y_data))]))
+
+    return Max_yarray,Min_yarray,Max_xarray,Min_xarray
     
 def get_discharge_information(folder_name):
     
-    info = folder_name.split("/")[1].split(".")[0].split("_")[1:5]
+    parameters = folder_name.split("/")[1].split(".")[0].split("_")[1:5]
+    d = '_'
+    info = d.join(parameters)
 
     return info 
 
@@ -38,37 +46,43 @@ def main():
     
     info = get_discharge_information(args.INFILE)
     
-    Results = pd.read_csv(args.INFILE,skiprows=10)
-    Data_Pandas = Results.iloc[:,2]
-    Data = np.asarray(Data_Pandas.values)
+    Results = pd.read_csv(args.INFILE)
+    xData_Pandas = Results.iloc[:,1]
+    x_data = np.asarray(xData_Pandas.values)
+    yData_Pandas = Results.iloc[:,2]
+    y_data = np.asarray(yData_Pandas.values)
 
     #pdb.set_trace()
 
     #a is the size of the sample of data over which we want to find the success rate
     a = args.INTERVAL
     
-    mod = len(Data)%a
+    mod = len(y_data)%a
 
 
     if mod == 0:
-        Max_array_Final,Min_Array_Final = Limit_Whole(Data,a)
+        Max_yarray_Final,Min_yArray_Final,Max_xarray_Final,Min_xarray_Final = Limit_Whole(x_data,y_data,a)
 
     else :
         
-        Data_1 = Data[0:-mod]
-        Data_2 = Data[-mod:]
+        y_data_1 = y_data[0:-mod]
+        x_data_1 = x_data[0:-mod]
+        y_data_2 = y_data[-mod:]
+        x_data_2 = x_data[-mod:]
         
-        Max_tab,Min_tab = Limit_Whole(Data_1,a)
+        Max_ytab,Min_ytab,Max_xtab,Min_xtab = Limit_Whole(x_data_1,y_data_1,a)
         
-        Max_array_Final,Min_array_Final = Limit_Partial(Data_2,a,Max_tab,Min_tab)
+        Max_yarray_Final,Min_yarray_Final,Max_xarray_Final,Min_xarray_Final = Limit_Partial(x_data_2,y_data_2,a,Max_tab,Min_tab)
     
-    Max_Array_Final = np.asarray(Max_array_Final)
-    Min_Array_Final = np.asarray(Min_array_Final)
-    
-    ydata_range = 20*np.arange(0,len(Max_Array_Final),1)
+    Max_yArray_Final = np.asarray(Max_yarray_Final)
+    Min_yArray_Final = np.asarray(Min_yarray_Final)
+    Max_xArray_Final = np.asarray(Max_xarray_Final)
+    Min_xArray_Final = np.asarray(Min_xarray_Final)
 
-    DATA = np.column_stack((ydata_range,Max_Array_Final,Min_Array_Final))
+    MAX_DATA = np.column_stack((Max_xArray_Final,Max_yArray_Final))
+    MIN_DATA = np.column_stack((Min_xArray_Final,Min_yArray_Final))
 
-    pd.DataFrame(DATA, columns = ['ID','Upper Limit','Lower Limit']).to_csv(os.path.join('Limits',"{}_{}_{}_{}_{}.csv".format(a,info[0],info[1],info[2],info[3])))
-        
+    pd.DataFrame(MAX_DATA, columns = ['x','y']).to_csv(os.path.join('Limits',"Max_{}".format(info)))
+    pd.DataFrame(MIN_DATA, columns = ['x','y']).to_csv(os.path.join('Limits','Min_{}'.format(info)))  
+
 main()
