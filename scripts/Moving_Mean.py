a6182abd00e88b91a18bd4a43dd43b2b09335a89
BourbeauN <68121560+BourbeauN@users.noreply.github.com>
2020-11-29 20:16:53 -0500

    Create Moving_Mean.py

---
+++ scripts/Moving_Mean.py
@@ -0,0 +1,72 @@
+import pdb
+import numpy as np
+import argparse
+import pandas as pd
+import os
+
+def Mean_Whole(Data,a):
+    index = np.arange(0,(len(Data)-(a-1)),a)
+    Mean_Value=[]
+    #pdb.set_trace()
+    for i in index:
+        #pdb.set_trace()
+        temp = ~np.isnan(Data[i:i+(a-1)])
+        mean = np.mean(temp)
+        Mean_Value.append(mean)
+    
+    return Mean_Value
+
+def Mean_Partial(Data,Success_Rate,a):
+    temp_data = ~np.isnan(Data)
+    mean = np.mean(temp_data)
+    Success_Rate.append(mean)
+    
+    return Success_Rate
+
+def get_discharge_information(folder_name):
+    
+    info = folder_name.split("/")[1].split(".")[0].split("_")[1:5]
+
+    return info 
+
+def main():
+    ###PARSER###
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', dest = 'INFILE', help = 'file folder corresponding to experimental set with discharge infos')
+    parser.add_argument('-i', type = int, dest = 'INTERVAL', help = 'size of data interval to compute probabilitmy calculation on')
+    args = parser.parse_args()
+    
+    info = get_discharge_information(args.INFILE)
+    
+    Results = pd.read_csv(args.INFILE,skiprows=10)
+    Data_Pandas = Results.iloc[:,2]
+    Data = np.asarray(Data_Pandas.values)
+
+    #pdb.set_trace()
+
+    #a is the size of the sample of data over which we want to find the success rate
+    a = args.INTERVAL
+    
+    mod = len(Data)%a
+
+    if mod == 0:
+        Final_Mean = Mean_Whole(Data,a)
+
+    else :
+        
+        Data_1 = Data[0:-mod]
+        Data_2 = Data[-mod:]
+        
+        Partial = Mean_Whole(Data_1,a)
+        
+        Final_Mean = Mean_Partial(Data_2,Partial,a)
+    
+    Final_Mean = np.asarray(Final_Mean)
+    
+    ydata_range = 20*np.arange(0,len(Final_Mean),1)
+
+    DATA = np.column_stack((ydata_range,Final_Mean))
+
+    pd.DataFrame(DATA, columns = ['ID','Mean']).to_csv(os.path.join('Moving_Mean',"{}_{}_{}_{}_{}.csv".format(a,info[0],info[1],info[2],info[3])))
+        
+main()
