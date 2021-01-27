a9ed680fe84a9b0a8f80f049cd1adf11e7ab0be3
nbourbeau <naomi.bourbeau@hotmail.com>
2021-01-27 10:24:04 -0500

    prediction

--- scripts/Prediction_fit.py
+++ scripts/Prediction_fit.py
@@ -10,16 +10,12 @@ from scipy.optimize import curve_fit
 
 
 def get_information(folder_name):
-    param = folder_name.split("/")[1].split(".")[0].split("_")[0:2]
-    filen = folder_name.split("/")[1].split(".")[0].split("_")[2:]
-    d = "_"
-    parameter = d.join(param)
-    fname = d.join(filen)
-
-    return parameter,fname
+    
+    fname = folder_name.split('/')[-1].split('.')[0]
+    return fname
 
 def Linear (x,a,b):
-    f = a*x + b
+    f = (a*x) + b
     
     return f
 
@@ -33,21 +29,37 @@ def main():
     
     #Importing data file as a matrix
     
-    Delay = pd.read_csv(args.f1)
-    Inj = pd.read_csv(args.f2)
-    y_data = Delay.iloc[:,2]
+    Delay = pd.read_csv(args.INFILE1)
+    Inj = pd.read_csv(args.INFILE2)
+    y_data = Delay['Plateau'].values
     x_data = np.arange(0,len(y_data),1)
     
-    y_data_d = Inj.iloc[:,2]
+    y_data_d = Inj.iloc[:,2].values
     x_data_d = np.arange(0,len(y_data_d),1)
     
-    parameter,fname = get_information(args.f1)
+    #pdb.set_trace()
+ 
+    fname = get_information(args.INFILE1)
     
-    popt_min,pcov_min = curve_fit(Linear,x_data,y_data,maxfev=10000)
+    data_noNan,x_noNan = [],[]    
+        
+    for i in range(len(y_data_d)):
+        if y_data[i]/y_data[i] == 1:
+            data_noNan.append(y_data[i])
+            x_noNan.append(x_data[i])
+
+    data_noNan = np.asarray(data_noNan)
+    x_noNan = np.asarray(x_noNan)    
+
+    popt,pcov = curve_fit(Linear,x_noNan,data_noNan,maxfev=10000)
+    lab = r'$%fx + %f$' % (popt[0],popt[1])
+    pdb.set_trace()
     
     plt.figure(1)
-    plt.plot(x_data_d,y_data_d,label='injected')
-    plt.plot(x_data,Linear(x_data,popt_min[1],popt_min[2]),linewidth=1,color = "salmon",label="curve fit")
+    plt.plot(x_data_d,y_data_d,label='injected',marker = '.',markersize = 1,color = 'crimson')
+    plt.plot(x_noNan,Linear(x_noNan,int(popt[0]),int(popt[1])),linewidth=0,marker = '.',markersize = 1,color = "salmon",label=lab)
     plt.legend()
     
-    plt.savefig(os.path.join("PLOTSPRED/{}/{}.pdf".format(parameter,fname)))
+    plt.savefig(os.path.join("PLOTSPRED/{}.pdf".format(fname)))
+
+main()
