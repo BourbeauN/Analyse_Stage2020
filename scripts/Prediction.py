6fd9aca64a7b4d01c85f28925ad08647c0c0f957
nbourbeau <naomi.bourbeau@hotmail.com>
2021-02-09 17:54:57 -0500

    New prediction parameter

--- scripts/Prediction.py
+++ scripts/Prediction.py
@@ -7,7 +7,8 @@ import pandas as pd
 
 def Lin_predict(mean_current,p_w,delay):
     
-    y = mean_current(p_w-delay)
+    new_t = p_w - delay    
+    y = mean_current*new_t
     
     return y
 
@@ -19,7 +20,7 @@ def get_information(filename):
     return info,p_w
 
 def main():
-    ###PARSER###
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-delay', dest = 'DELAY', help = 'file folder corresponding to experimental set with discharge infos')
     parser.add_argument('-mean', dest = 'MEAN', help = 'pick a value for voltage threshold')
@@ -29,12 +30,21 @@ def main():
     Delay_data = pd.read_csv(args.DELAY)
     Mean_data = pd.read_csv(args.MEAN)
     Inj_data = pd.read_csv(args.INJECTED)
+
+    delay_d = Delay_data["Plateau"]
+    mean_d = Mean_data["Mean"]
+    inj_d = Inj_data["Absolute_Injected_Charges"]
     
-    delay = Delay_data["Plateau"].values()
-    mean = Mean_data["Mean"].values()
-    inj= Inj_data["Injected_Charges"].values()
-    
-    pred_y = Lin_predict(mean,p_w,delay)
+    delay = np.asarray(delay_d.values)
+    mean = np.asarray(mean_d.values)
+    inj = np.asarray(inj_d.values)
+
+    info,p_w = get_information(args.MEAN)
+
+    p_w_array = np.zeros(len(mean))
+
+    p_w_array += p_w*1e-9
+    pred_y = Lin_predict(mean,p_w_array,delay)
     
     pred_y = np.asarray(pred_y)
   
