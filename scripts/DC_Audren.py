b70ec7b296f1e9b6735db8dfa4324231b060c2fc
BourbeauN <68121560+BourbeauN@users.noreply.github.com>
2021-05-28 14:14:18 -0400

    Update DC_Audren.py

--- scripts/DC_Audren.py
+++ scripts/DC_Audren.py
@@ -16,36 +16,20 @@ def load_data(filename):
     
     return np.array(time), np.array(voltage), np.array(current) 
 
-def load_BV(filename):
-    
-    Results = pd.read_csv(filename,skiprows=1)
-    BV = Results.iloc['Voltage']
-    
-    return BV   
-
-def max_current(path1,path2):
+def max_current(path):
     
     Current_Tab = []    
     count = 0
     # list of discharge files  
-    files1 = sorted(os.listdir(path1))
+    files = sorted(os.listdir(path))
     
-    for i, f in enumerate(files1) :
-        time, voltage, current = load_data(os.path.join(path1,f))
-        pdb.set_trace() 
-        BV = load_BV(path2)
+    for i, f in enumerate(files) :
+        time, voltage, current = load_data(os.path.join(path,f))
+        max_value = np.amax(current)
         
-        if BV[i] > 0 :
-            max_value = np.amax(current)
-        
-        elif BV[i] < 0:
-            max_value = np.amin(current)
-    
         if count%100 == 0:
             print(count)
-       
-        pdb.set_trace()
- 
+        
         count += 1
 
         Current_Tab.append([f,max_value])
@@ -56,11 +40,10 @@ def main ():
     
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', dest = 'INFILE', help = 'discharge file')
-    parser.add_argument('-b', dest = 'BREAKDOWN', help = 'breakdown voltage file')
     args = parser.parse_args()
     outfile = args.INFILE.split('/')[-1] 
 
-    MAX_CURRENT_TAB = max_current(args.INFILE,args.BREAKDOWN)
+    MAX_CURRENT_TAB = max_current(args.INFILE)
 
     pd.DataFrame(MAX_CURRENT_TAB, columns = ['Filename', 'Max Current']).to_csv(os.path.join('Max_Current','{}.csv'.format(outfile))) 
 
