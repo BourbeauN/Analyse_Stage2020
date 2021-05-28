ae326a28361fd323252b13615a4752f383ddcfec
BourbeauN <68121560+BourbeauN@users.noreply.github.com>
2021-05-28 13:47:16 -0400

    Create DC_Audren.py

---
+++ scripts/DC_Audren.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import argparse
+import numpy as np
+import os
+import pdb
+
+##Data loading function
+
+def load_data(filename):    
+    
+    Results = pd.read_csv(filename, skiprows = 10)
+
+    time = Results['TIME']
+    voltage = Results['CH1']
+    current = Results['CH2']
+    
+    return np.array(time), np.array(voltage), np.array(current) 
+
+def max_current(path):
+    
+    Current_Tab = []    
+    count = 0
+    # list of discharge files  
+    files = sorted(os.listdir(path))
+    
+    for i, f in enumerate(files) :
+        time, voltage, current = load_data(os.path.join(path,f))
+        max_value = np.amax(current)
+        
+        if count%100 == 0:
+            print(count)
+        
+        count += 1
+
+        Current_Tab.append([f,max_value])
+        
+    return np.asarray(Current_Tab)
+
+def main ():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', dest = 'INFILE', help = 'discharge file')
+    args = parser.parse_args()
+    outfile = args.INFILE.split('/')[-1] 
+
+    MAX_CURRENT_TAB = max_current(args.INFILE)
+
+    pd.DataFrame(MAX_CURRENT_TAB, columns = ['Filename', 'Max Current']).to_csv(os.path.join('Max_Current','{}.csv'.format(outfile))) 
+
+main()
